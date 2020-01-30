e5a4936c26d71e21eab1d18cb1cc6a78cdffb1b3
kalebe321 <59872710+kalebe321@users.noreply.github.com>
2020-01-30 09:02:28 -0300

    d

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,14 @@
+function vogalOuConsoante(letra){
+    const letras = letra.toLowerCase()
+    
+    if(letras === "a" || letras === "e" || letras === "i" || letras === "o" || letras === "u"){
+        return("Vogal")
+    }
+    else{
+        return("Consoante")
+    }
+}
+
+
+
+
