470ac0deeda64fab9384cc07ee939b9c9b4774a8
kalebe321 <59872710+kalebe321@users.noreply.github.com>
2020-02-06 11:11:46 -0300

    a

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,14 @@
+function ateZero(numero){
+    var a = 0
+    while(a <= numero){
+        console.log(a)
+        a++
+    }
+    if(numero <= 0){
+    while(a >= numero){
+        console.log(numero)
+        numero++
+    }
+  }
+}
+
