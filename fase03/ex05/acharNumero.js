c968cf7f632a8d4c061c7e418dbc556edfec1de3
kalebe321 <59872710+kalebe321@users.noreply.github.com>
2020-02-06 09:07:40 -0300

    d

--- fase03/ex05/acharNumero.js
+++ fase03/ex05/acharNumero.js
@@ -1,15 +1,16 @@
-function acharNumero(num){
-    const numero = num
-    if(numero < 5){
-        return("mercurio")
+
+function acharNumero ( num ) {
+    const  numero  =  num
+    if ( numero  <  5 ){
+        return( "mercurio" )
     }
-    else if(numero >=5 && numero <=10){
-        return("venus")
+     else if(numero >= 5  &&  numero <= 10){
+        return ( "venus" )
     }
-    else if(numero >10 && numero <=100){
-        return("terra")
+     else if(numero  > 10 &&  numero <= 100) {
+        return ("terra")
     }
     else{
-        return("marte")
+        return ("marte")
     }
 }
