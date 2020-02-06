0e34cd8afe6607cf196fb6b00a588e0ce1042d31
kalebe321 <59872710+kalebe321@users.noreply.github.com>
2020-02-06 09:27:35 -0300

    l

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,12 +1,12 @@
-function  parOuImpar(num,num2){
+function  pares(num,num2){
     var numero = num % 2 ;
     var numero2 = num2 % 2;
     var total = numero || numero2;
     
     if(total === 0){
-        return( "Os numeros s„o pares")
+        return( "Os numeros s√£o pares")
     }
     else{
-        return("Os numeros n„o s„o pares")
+        return("Os numeros n√£o s√£o pares")
     }
 }
