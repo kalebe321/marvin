d86689d9f657db6609b4306e5b8394b12631ad29
kalebe321 <59872710+kalebe321@users.noreply.github.com>
2020-02-06 11:09:54 -0300

    s

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,7 +1,14 @@
-function entreNumeros(min,max){
-    var a = min;
-    while(a <= max){
+function ateZero(numero){
+    var a = 0
+    while(a <= numero){
         console.log(a)
         a++
     }
+    if(numero <= 0){
+    while(a >= numero){
+        console.log(numero)
+        numero++
+    }
+  }
 }
+
