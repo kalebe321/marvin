c555de6d9b535510e5921ae584ede58b5322a445
kalebe321 <59872710+kalebe321@users.noreply.github.com>
2020-02-06 08:53:02 -0300

    q

--- fase03/ex02/pares.js
+++ fase03/ex02/pares.js
@@ -1,10 +1,10 @@
-function parOuImpar(num,num2){
-    var numero1 = num;
-   
-    if(numero1 % 2 == 0){
-        return("Os numeros sao pares")
+function  parOuImpar(num,num2) {
+    var  numero1  =  num;
+    
+    if(numero1 % 2 === 0){
+        return ("Os numeros são pares")
     }
     else{
-        return("Os numeros nao sao pares")
+        return ("Os numeros não são pares")
     }
 }
