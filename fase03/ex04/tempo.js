f9fecf1a0839c3e655012434f2c06a3bbca5aafa
kalebe321 <59872710+kalebe321@users.noreply.github.com>
2020-02-06 10:40:40 -0300

    l

--- fase03/ex04/tempo.js
+++ fase03/ex04/tempo.js
@@ -2,19 +2,21 @@ function tempo(data1, data2, unidade){
 var inicio = new Date(data1);
 var final = new Date(data2);
 switch(unidade){
+    
     case "dia":
     var periodo = Math.abs(inicio.getTime() - final.getTime())/1000/60/60/24;
-    return Math.round(periodo);
+    return Math.round(periodo)
     break;
-    case "mes":
+    
+    case "mês":
     var periodo1 = Math.abs(inicio.getTime() - final.getTime())/1000/60/60/24/30;
-    return Math.round(periodo1);
+    return Math.round(periodo1)
+    break;
     default:
-     var periodo2 = Math.abs(inicio.getTime() - final.getTime())/1000/60/60/24/30/12;
-    return Math.round(periodo2);
+    var periodo2 = Math.abs(inicio.getTime() - final.getTime())/1000/60/60/24/30/12;
+    return Math.round(periodo2)
     break;
 }
 
 }
-
 
