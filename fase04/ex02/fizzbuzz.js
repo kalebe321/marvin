cfd05b0b7e0b9773cfd5bde6bc1b55e8b333923a
kalebe321 <59872710+kalebe321@users.noreply.github.com>
2020-02-11 11:09:00 -0300

    s

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -1,19 +1,22 @@
 function fizzbuzz(num1,num2){
     var c = num1
     while(c <= num2){
-           if(c % 3 === 0){
-        console.log(c + "Fizz")
-        c++
-    }
-    if(c % 5 === 0){
-        console.log(c + "Buzz")
-        c++
-    }
     if(c % 3 === 0 && c % 5 === 0){
-     console.log( + "FizzBuzz")
-     c++
+     console.log(c + "FizzBuzz")
+     
     }
-    console.log(c)
+    else if(c % 5 === 0){
+        console.log(c + "Buzz")
+        
+    }
+    else if(c % 3 === 0){
+        console.log(c + "Fizz")
+        
+    }
+    else{
+        console.log(c)
+        
+    }
     c++
-    }
+    }          
 }
